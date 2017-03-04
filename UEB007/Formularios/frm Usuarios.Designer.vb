234e5637b4e99e081f9c47373faef1104d91b6c9
jaur121 <jaur777@hotmail.com>
2017-03-04 12:04:17 -0600

    subo el formulario de Usuarios

---
+++ UEB007/Formularios/frm Usuarios.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class pnlRoles
+    Inherits Syncfusion.Windows.Forms.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlMenu = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.GridUsuarios = New System.Windows.Forms.DataGridView()
+        CType(Me.GridUsuarios, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pnlMenu
+        '
+        Me.pnlMenu.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlMenu.Location = New System.Drawing.Point(0, 0)
+        Me.pnlMenu.Name = "pnlMenu"
+        Me.pnlMenu.Size = New System.Drawing.Size(656, 65)
+        Me.pnlMenu.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel2.Location = New System.Drawing.Point(456, 65)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(200, 304)
+        Me.Panel2.TabIndex = 1
+        '
+        'GridUsuarios
+        '
+        Me.GridUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GridUsuarios.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GridUsuarios.Location = New System.Drawing.Point(0, 65)
+        Me.GridUsuarios.Name = "GridUsuarios"
+        Me.GridUsuarios.Size = New System.Drawing.Size(456, 304)
+        Me.GridUsuarios.TabIndex = 2
+        '
+        'pnlRoles
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(656, 369)
+        Me.Controls.Add(Me.GridUsuarios)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.pnlMenu)
+        Me.Name = "pnlRoles"
+        Me.ShowIcon = False
+        Me.Text = "Administriar Usuarios"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.GridUsuarios, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlMenu As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents GridUsuarios As DataGridView
+End Class
---
+++ UEB007/Formularios/frm Usuarios.vb
@@ -0,0 +1,9 @@
+﻿Imports Syncfusion.Windows.Forms
+
+Public Class pnlRoles
+    Inherits MetroForm
+
+    Private Sub pnlRoles_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
